7ca455bad795c1007b5a1686308d1762a0381d59
julistri <stritzkejulian@gmail.com>
2025-12-28 16:38:10 +0100

    change port, add .env

--- server.py
+++ server.py
@@ -2,14 +2,21 @@
 
 from mcp.server.fastmcp import FastMCP
 from tavily import TavilyClient
+from dotenv import load_dotenv
 from typing import Dict
+import os
+
+load_dotenv()
+TAVILY_API_KEY = os.environ["TAVILY_API_KEY"]
+if "TAVILY_API_KEY" not in os.environ:
+    raise Exception("TAVILY_API_KEY environment variable not set")
 
 # Initialize Tavily client with your API key
-# Replace "TAVILY_API_KEY" with your actual key
-tavily_client = TavilyClient("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")
+tavily_client = TavilyClient(TAVILY_API_KEY)
 
+PORT = os.environ.get("PORT", 10000)
 # Create an MCP server named "mcpLapengineer"
-mcp = FastMCP("mcpLapengineer", host="0.0.0.0", port=8000)
+mcp = FastMCP("mcpLapengineer", host="0.0.0.0", port=PORT)
 
 # Add a tool that uses Tavily for web searches
 @mcp.tool()
