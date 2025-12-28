411fc89faa4f5526d5f26d532f2749b9e5ccb08b
julistri <stritzkejulian@gmail.com>
2025-12-28 20:42:44 +0100

    update server.py

--- server.py
+++ server.py
@@ -1,26 +1,29 @@
-# server.py
 
 from mcp.server.fastmcp import FastMCP
 from tavily import TavilyClient
 from dotenv import load_dotenv
-from typing import Dict
+from typing import Dict, List
 import os
 
 load_dotenv()
-TAVILY_API_KEY = os.environ["TAVILY_API_KEY"]
+
 if "TAVILY_API_KEY" not in os.environ:
     raise Exception("TAVILY_API_KEY environment variable not set")
+  
+# Tavily API key
+TAVILY_API_KEY = os.environ["TAVILY_API_KEY"]
 
-# Initialize Tavily client with your API key
+# Initialize Tavily client
 tavily_client = TavilyClient(TAVILY_API_KEY)
 
 PORT = os.environ.get("PORT", 10000)
-# Create an MCP server named "mcpLapengineer"
-mcp = FastMCP("mcpLapengineer", host="0.0.0.0", port=PORT)
 
-# Add a tool that uses Tavily for web searches
+# Create an MCP server
+mcp = FastMCP("web-search", host="0.0.0.0", port=PORT)
+
+# Add a tool that uses Tavily
 @mcp.tool()
-def web_search(query: str) -> Dict:
+def web_search(query: str) -> List[Dict]:
     """
     Use this tool to search the web for information.
 
@@ -33,8 +36,8 @@ def web_search(query: str) -> Dict:
     try:
         response = tavily_client.search(query)
         return response["results"]
-    except Exception:
-        return "No results found"
+    except Exception as e:
+        return "Error: " + str(e)
 
 # Run the server
 if __name__ == "__main__":
