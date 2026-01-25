7c4f0815c98ee08bb71d6ae652ed8ca368e1cee8
julistri <stritzkejulian@gmail.com>
2026-01-25 21:04:12 +0100

    Revert "change transport, to fix shutdowns"
    This reverts commit 8fc63ac6df007d4e54f00aa5feac2a66675aefbd.

--- server.py
+++ server.py
@@ -381,5 +381,4 @@ def validate_select_query(query: str):
 
 # Run the server
 if __name__ == "__main__":
-    #mcp.run(transport="streamable-http")
-    mcp.run(transport="http")
+    mcp.run(transport="streamable-http")
